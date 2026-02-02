e466f799cfe58f624ce7638124b8e5d7b945babf
bluefinsteam <bluefinsteam@gmail.com>
2026-02-02 10:44:47 +0700

    Create FO_Sales with Transactions.sql
    Commit Sales with Transaction

---
+++ FO_Sales with Transactions.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM PROJECT_1.dbo.Transaction_FO tf 
+INNER JOIN syndpdev001.dbo.tb_SalesInvoice tsi 
+ON tf.CompanyCode = tsi.SysCompanyId 
+AND tf.[Ref.DocNum] = tsi.SalesId 
+AND tf.[FO-ITEMNO] = tsi.ItemId 
+WHERE tf.CompanyCode = 'bnn'
+--AND tf.INVENTLOCATIONID = '79006'
+AND tsi.InvoiceDate >= '2026-01-01' --Date from
+AND tsi.InvoiceDate <= '2026-01-31' --Date to
+ORDER BY tsi.SalesId ASC, tsi.[LineNo] ASC
