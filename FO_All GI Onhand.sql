8f549cc27a79bddde9bff0ed21f42562b93b972a
mis-tan <rattaporn.w@comseven.com>
2026-02-10 11:52:22 +0700

    Create FO_All GI Onhand.sql
    Join with GroupCat, Cat, SubCat

---
+++ FO_All GI Onhand.sql
@@ -0,0 +1,20 @@
+SELECT 
+	i.dataareaid,
+	i.inventlocationid, 
+	i.itemid,ti.ItemName, 
+	i.inventserialid, 
+	i.availphysical, 
+	ti.[Material Code], 
+	ti.ReferenceId , 
+	ti.[Search Name], 
+	tfdvs.GroupCategoryName,
+	tfdvs.CategoryName,
+	tfdvs.SubCategoryName 
+FROM D365FO_DATALAKE.dbo.inventsum as i 
+LEFT JOIN syndpdev001.dbo.tm_Item ti 
+LEFT JOIN syndpdev001.dbo.tm_FinDimValueSet tfdvs 
+ON ti.DefaultDimension = tfdvs.FinancialDim 
+ON i.itemid = ti.ItemId AND ti.SysCompanyId = 'GI'
+WHERE i.dataareaid LIKE '%GI%'
+AND i.availphysical <> 0
+ORDER BY i.itemid , i.dataareaid, i.inventlocationid ASC
